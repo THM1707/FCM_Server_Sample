c1c2425f2d877a0fae14a879137e0d42d3062edd
THM1707 <tranhoangminh1707@gmail.com>
2018-03-20 23:08:59 +0700

    Temperatory

--- app/controllers/firebase_controller.rb
+++ app/controllers/firebase_controller.rb
@@ -2,26 +2,28 @@ class FirebaseController < ApplicationController
   require 'fcm'
 
   def send_to_topic
-    topic = params["topic"]
-    message_body = params["body"]
-    message_title = params["title"]
+    topic = params['topic']
+    message_body = params['body']
+    message_title = params['title']
     fcm = FCM.new(Constants::KEY)
     response = fcm.send_to_topic(topic, notification: {body: message_body, title: message_title})
     render json: response, status: :ok
   end
 
-  def send_to_device
-    # token = params["token"]
-    # message_body = params["body"]
-    # message_title = params["title"]
-    # option = {notification: {body: message_body, title: message_title}}
-    # fcm = get_fcm
-    # response = fcm.send(token, option)
-    # render json: response, status: :ok
+  def send_topic_data
+    topic = params['topic']
+    message = params['message']
+    title = params['title']
+    image_url = params['image']
+    timestamp = params['timestamp']
     fcm = FCM.new(Constants::KEY)
-    response = fcm.send(["eIm5ue6s1aU:APA91bFnpJOUai2W8RzuZOVtOG5cZNyJxa94SE953HoLk_iato77bsIScgIOuBNTH3519ZwgTaAHMm0cAoNGvBiTDUfrJ_C-v2BQ4gbyu7GmrZdUH0W-LniXmJi_vrQXR0nbQ2qBxKHi"],
-                        notification: {body: "Test", title: "Test message"})
+    response = fcm.send_to_topic(topic,
+                                 data: {message: message,
+                                        title: title,
+                                        image: image_url,
+                                        timestamp: timestamp
+                                 },
+                                 collapse_key: 'picture')
     render json: response, status: :ok
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'notification/topic', to: 'firebase#send_to_topic'
-  post 'notification/device', to: 'firebase#send_to_device'
+  post 'notification/data', to: 'firebase#send_topic_data'
 end
